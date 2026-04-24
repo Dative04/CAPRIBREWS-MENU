e67b787d52ad1504d64292ab7b4ac1babfa6a39d
CARLOS MULLER <carlossmuller@iskolarngbayan.pup.edu.ph>
2026-04-24 16:21:31 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase configuration for Capibrews Menu
 const firebaseConfig = {
-  apiKey: "AIzaSyD872nPE_rH1akzVLexVWVtaNEyGSZvG1U", // Your API Key
-  authDomain: "capibrews-menu.firebaseapp.com", // Your Auth Domain
-  projectId: "capibrews-menu", // Your Project ID
-  storageBucket: "capibrews-menu.firebasestorage.app", // Your Storage Bucket
-  messagingSenderId: "492802116750", // Your Messaging Sender ID
-  appId: "1:492802116750:web:185de5fbc03f477753cddf", // Your App ID
-  measurementId: "G-7T1B0Y9RZV" // Your Measurement ID
+  apiKey: "AIzaSyD872nPE_rH1akzVLexVWVtaNEyGSZvG1U",
+  authDomain: "capibrews-menu.firebaseapp.com",
+  projectId: "capibrews-menu",
+  storageBucket: "capibrews-menu.firebasestorage.app",
+  messagingSenderId: "492802116750",
+  appId: "1:492802116750:web:185de5fbc03f477753cddf",
+  measurementId: "G-7T1B0Y9RZV"
 };
 
 // Initialize Firebase using the "Compat" version
