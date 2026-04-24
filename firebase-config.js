ff026b89ca93d40da5bfd0e02aece275771c4b47
CARLOS MULLER <carlossmuller@iskolarngbayan.pup.edu.ph>
2026-04-24 16:19:48 +0800

    Refactor Firebase config and initialization method
    Updated Firebase configuration with comments and changed initialization method to use Compat version.

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration for Capibrews Menu
 const firebaseConfig = {
-  apiKey: "AIzaSyD872nPE_rH1akzVLexVWVtaNEyGSZvG1U",
-  authDomain: "capibrews-menu.firebaseapp.com",
-  projectId: "capibrews-menu",
-  storageBucket: "capibrews-menu.firebasestorage.app",
-  messagingSenderId: "492802116750",
-  appId: "1:492802116750:web:185de5fbc03f477753cddf",
-  measurementId: "G-7T1B0Y9RZV"
+  apiKey: "AIzaSyD872nPE_rH1akzVLexVWVtaNEyGSZvG1U", // Your API Key
+  authDomain: "capibrews-menu.firebaseapp.com", // Your Auth Domain
+  projectId: "capibrews-menu", // Your Project ID
+  storageBucket: "capibrews-menu.firebasestorage.app", // Your Storage Bucket
+  messagingSenderId: "492802116750", // Your Messaging Sender ID
+  appId: "1:492802116750:web:185de5fbc03f477753cddf", // Your App ID
+  measurementId: "G-7T1B0Y9RZV" // Your Measurement ID
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Initialize Firebase using the "Compat" version
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
